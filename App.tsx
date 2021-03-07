c47696b3fe1f731c5a3a5c8c752ec8b7841fb9b9
Matthew Mitchener <mattjmitchener@gmail.com>
2021-03-07 16:02:58 -0500

    Reproduce tab nav issue

--- App.tsx
+++ App.tsx
@@ -1,21 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+const AppContext = React.createContext({});
+
+const ModalScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Modal</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeScreen = ({ navigation }: any) => {
+  const { text }: any = React.useContext(AppContext);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Your text: {text}</Text>
+      <Button title="Open modal" onPress={() => navigation.navigate("Modal")} />
+      <Button
+        title="Open settings"
+        onPress={() => navigation.navigate("Settings")}
+      />
+    </View>
+  );
+};
+
+const ProfileScreen = () => {
+  const { text, setText }: any = React.useContext(AppContext);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button
+        title="Changing state inside tab screen should not redirect us to home"
+        onPress={() => setText(text === "hello" ? "hi" : "hello")}
+      />
+    </View>
+  );
+};
+
+const SettingsScreen = () => {
+  const { text, setText }: any = React.useContext(AppContext);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button
+        title="Changing state inside Screen will not redirect us"
+        onPress={() => setText(text === "hello" ? "hi" : "hello")}
+      />
+    </View>
+  );
+};
+
+export default function App() {
+  const [text, setText] = React.useState("");
+
+  const Root = createStackNavigator();
+  const Main = createStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  const TabStack = () => {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    );
+  };
+
+  const MainNav = () => {
+    return (
+      <Main.Navigator>
+        <Main.Screen name="HomeTabs" component={TabStack} />
+        <Main.Screen name="Settings" component={SettingsScreen} />
+      </Main.Navigator>
+    );
+  };
+
+  return (
+    <AppContext.Provider value={{ text, setText }}>
+      <NavigationContainer>
+        <Root.Navigator mode="modal">
+          <Root.Screen name="MainNav" component={MainNav} />
+          <Root.Screen name="Modal" component={ModalScreen} />
+        </Root.Navigator>
+      </NavigationContainer>
+    </AppContext.Provider>
+  );
+}
